3287239a578164e9d2991e2defc3a9e082d15f87
Q <qiulin.li319@gmail.com>
2025-11-24 16:05:18 +0000

    Adding spectral stuff

--- Preprocessing/Generate_stability_metrics.py
+++ Preprocessing/Generate_stability_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import kurtosis
+from scipy.fft import fft
 
 df=pd.read_csv('data/EVI_PID.csv')
 pid=pd.read_csv('data/lookup/PID_location.csv')
@@ -8,6 +9,7 @@ pid=pd.read_csv('data/lookup/PID_location.csv')
 AR1=[]
 Temp_kurt=[]
 pids=[]
+spectral_sd_list=[]
 
 for pid in pid['PID']:
 
@@ -25,14 +27,16 @@ for pid in pid['PID']:
     dates=sub.columns.tolist()
     evi=np.array(sub)
     sub1=pd.Series(evi.flatten(), index=pd.to_datetime(dates))
-
+    spectral_sd = np.std(fft(evi)[1:])
+           
     lag_cor =sub1.autocorr()  
     k = kurtosis(sub1, fisher=True, bias=False)
 
     pids.append(pid)
     Temp_kurt.append(k)
     AR1.append(lag_cor)
+    spectral_sd_list.append(spectral_sd)
 
 
-df=pd.DataFrame({'PID': pids, 'AR1': AR1, 'Kurtosis': Temp_kurt})
+df=pd.DataFrame({'PID': pids, 'AR1': AR1, 'Kurtosis': Temp_kurt, 'Temp Spectral SD':spectral_sd_list})
 df.to_csv('data/temp_stability_metrics.csv', index=False)
--- other/evi_vis.py
+++ other/evi_vis.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import kurtosis
 import matplotlib.pyplot as plt
-
+from scipy.fft import fft
 
 df=pd.read_csv('data/EVI_PID.csv')
 pid=pd.read_csv('data/lookup/PID_location.csv')
@@ -17,14 +17,20 @@ sub.drop(columns=['PID'], inplace=True)
 
 dates=sub.columns.tolist()
 evi=np.array(sub)
-sub1=pd.Series(evi.flatten(), index=pd.to_datetime(dates))
+evi=evi.flatten()
+
+sub1=pd.Series(evi, index=pd.to_datetime(dates))
 
 pearson =sub1.autocorr()  
 k = kurtosis(sub1, fisher=True, bias=False)
 
-
-print(k)
 plt.figure(figsize=(20,5))
-plt.plot(dates, evi.flatten())
-plt.title(f"EVI Time Series for PID: {pid} with AR(1)={pearson}", fontsize=16)
+plt.plot(dates, evi)
+plt.title(f"EVI Time Series for PID: {pid} with AR(1)={pearson} and kurtosis={k}", fontsize=16)
+
+plt.figure(figsize=(15,8))
+plt.plot(dates[1:], fft(evi)[1:])
+plt.title(f" FFT of EVI for PID: {pid} with std={np.std(fft(evi)[1:])} ", fontsize=16)
+
+
 plt.show()
