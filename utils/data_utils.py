73f99263dca5d739fe7ad9e80d0a0e20b857bc09
“Qiulin” <qiulin.li.25@ucl.ac.uk>
2026-04-22 06:40:47 +0100

    fixes in merging that caused data loss

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -27,7 +27,7 @@ def merge_files(csv_dir, out_file):
     stacked.to_parquet(out_file, engine="pyarrow", compression="snappy")
     shutil.rmtree(csv_dir)  
 
-    
+
 def data_preprocessing(df, npp_df, csc_df):
     """
     Cleaning and merging datasets for ML models
@@ -35,6 +35,8 @@ def data_preprocessing(df, npp_df, csc_df):
 
     y_df=npp_df.merge(csc_df,  on='PID', how='inner')
     y_df.rename(columns={"mean": "npp"}, inplace=True)
+
+    print(y_df.shape)
     # y_df.drop(columns=['Unnamed: 0_x', 'Unnamed: 0_y'], inplace=True)
 
     # df = pd.get_dummies(df, columns=['biome', 'ownership'])
@@ -47,13 +49,16 @@ def data_preprocessing(df, npp_df, csc_df):
 
     fd_df=df.copy()
     fd_df.drop(columns=['Species Richness', 'Shannon Diversity', "Simpson's Index", "Shannon Equitabiltiy Index", "source_file",  "Functional_Dispersion", "Functional_Divergences"], inplace=True)
-    fd_df=fd_df.merge(y_df, on='PID')
+    fd_df=fd_df.merge(y_df, on='PID', how='inner')
 
-    fd_df=fd_df.dropna()
+    fd_df = fd_df.drop(columns=[col for col in fd_df.columns if "_y" in col])
+    fd_df.columns = fd_df.columns.str.replace('_x$', '', regex=True)
+    fd_df.dropna(subset=['Functional_Evenness', "Mean Pairwise D"], inplace=True)
+
     sd_df=df.copy()
     sd_df.drop(columns=['Raos_Q', 'Functional_Evenness', "Functional_Dispersion", "Functional_Divergences", "Mean Pairwise D", "Shannon Equitabiltiy Index", "source_file"], inplace=True)
-    sd_df=sd_df.merge(y_df, on='PID')
-    sd_df.dropna(axis=0, inplace=True)
+    sd_df=sd_df.merge(y_df, on='PID', how='inner')
+    sd_df.dropna(subset=['Species Richness', 'Shannon Diversity', "Simpson's Index"], inplace=True)
 
     return sd_df, fd_df
 
